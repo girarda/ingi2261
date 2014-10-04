7b9b5fdb50db5fb37fadf90b01b204bf898210f1
Alexandre Girard <girarda.92@gmail.com>
2014-10-04 08:46:33 -0700

    use dict + deepish copy.

--- koutack/koutack.py
+++ koutack/koutack.py
@@ -8,13 +8,19 @@ import copy
 class Koutack(Problem):
 
     def __init__(self,init):
-        self.initial = [line.split() for line in open(init)]
+        self.initial = tuple([line.split() for line in open(init)])
+        self.height = len(self.initial)
+        self.width = len(self.initial[0])
+        d = {}
         for i in range(len(self.initial)):
             for j in range(len(self.initial[0])):
                 if self.initial[i][j] == '.':
-                    self.initial[i][j] = []
+                    self.initial[i][j] = ()
+                    d[(i,j)] = ()
                 else:
-                    self.initial[i][j] = list(self.initial[i][j])
+                    self.initial[i][j] = tuple(self.initial[i][j])
+                    d[(i, j)] = tuple(self.initial[i][j])
+        self.initial = d
 
         ## Sum of piles = 1
         self.goal = 1
@@ -23,34 +29,30 @@ class Koutack(Problem):
         return self.get_number_pile(state) == 1
 
     def successor(self, state):
-        for i in range(len(state)):
-            for j in range(len(state[0])):
+        for i in range(self.height):
+            for j in range(self.width):
                 if(self.is_empty(state, i, j)):
                     if self.can_merge(state, i, j):
-                        yield "ACTION!", self.merge(state, i, j)        
+                        yield "ACTION!", self.merge(state, i, j)
+
     def get_number_pile(self, state):
-        count = 0
-        for line in state:
-            for c in line:
-                if len(c) > 0:
-                    count+=1
-        return count
+        return len((list(x for x in state.values() if len(x) > 0)))
 
     def is_empty(self, state, x, y):
-        return len(state[x][y]) == 0
+        return len(state[(x,y)]) == 0
 
     def count_adjacent_piles(self, state, x, y):
         count = 0
         if x > 0:
             if not self.is_empty(state, x-1, y):
                 count +=1
-        if x < len(state)-1:
+        if x < self.height-1:
             if not self.is_empty(state, x+1, y):
                 count +=1
         if y > 0:
             if not self.is_empty(state, x, y-1):
                 count +=1
-        if y < len(state)-1:
+        if y < self.width-1:
             if not self.is_empty(state, x, y+1):
                 count +=1
         return count
@@ -59,23 +61,23 @@ class Koutack(Problem):
         return self.count_adjacent_piles(state, x, y) >= 2
 
     def merge(self, state, x, y):
-        newState = copy.deepcopy(state)
+        newState = deepish_copy(state)
         if x > 0:
             if not self.is_empty(state, x-1, y):
-                newState[x][y] += state[x-1][y]
-                newState[x-1][y] = []
-        if x < len(state)-1:
+                newState[x, y] += state[x-1, y]
+                newState[x-1, y] = ()
+        if x < self.height-1:
             if not self.is_empty(state, x+1, y):
-                newState[x][y] += state[x+1][y]
-                newState[x+1][y] = []
+                newState[x, y] += state[x+1, y]
+                newState[x+1, y] = ()
         if y > 0:
             if not self.is_empty(state, x, y-1):
-                newState[x][y] += state[x][y-1]
-                newState[x][y-1] = []
-        if y < len(state[0])-1:
+                newState[x, y] += state[x, y-1]
+                newState[x, y-1] = ()
+        if y < self.width-1:
             if not self.is_empty(state, x, y+1):
-                newState[x][y] += state[x][y+1]
-                newState[x][y+1] = []
+                newState[x, y] += state[x, y+1]
+                newState[x, y+1] = ()
         return newState
 
 def printSolution(path):
@@ -95,17 +97,28 @@ def printSolution(path):
                             grid += ","
                     grid += "] "
             print(grid)
-        print("\n")
+        print("")
+
+def deepish_copy(org):
+    '''
+    much, much faster than deepcopy, for a dict of the simple python types.
+    '''
+    out = dict().fromkeys(org)
+    for k,v in org.items():
+        try:
+            out[k] = v.copy()   # dicts, sets
+        except AttributeError:
+            try:
+                out[k] = v[:]   # lists, tuples, strings, unicode
+            except TypeError:
+                out[k] = v      # ints
+ 
+    return out
 
 ###################### Launch the search #########################
-problem=Koutack(sys.argv[1])
-#succ = problem.successor(problem.initial)
-#for act, state in succ:
-    #print(state, '\n')
-#print(problem.initial)
-#print(problem.goal_test(problem.initial))
-node=depth_limited_search(problem)
-path=node.path()
-path.reverse()
+# problem=Koutack(sys.argv[1])
+# node=breadth_first_tree_search(problem)
+# path=node.path()
+# path.reverse()
 
-printSolution(path)
+# printSolution(path)
